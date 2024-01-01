1b8429675836abcb9a69faeed633d794ea7bec6f
hashinivish <e19142@eng.pdn.ac.lk>
2024-01-01 22:53:53 +0530

    buttons

--- Website/src/pages/Dashboard.jsx
+++ Website/src/pages/Dashboard.jsx
@@ -1,11 +1,8 @@
 import logo from '/img/PALLETPRO.png';
 import { Flex } from '@chakra-ui/react';
-import { Grid, GridItem, Text, Image, Stack} from '@chakra-ui/react'
-import { Button, ButtonGroup } from '@chakra-ui/react'
-import { EmailIcon } from '@chakra-ui/icons';
-import { FaSignInAlt } from 'react-icons/fa';
-
-
+import { Grid, GridItem, Text, Image, Stack } from '@chakra-ui/react';
+import { Button } from '@chakra-ui/react';
+import { FaSignInAlt, FaUserPlus } from 'react-icons/fa';
 
 export default function Dashboard() {
   return (
@@ -18,26 +15,47 @@ export default function Dashboard() {
       color='blackAlpha.700'
       fontWeight='bold'
     >
-      <GridItem  bg='#140d07' area={'header'}>
+      <GridItem bg='#140d07' area={'header'}>
         Header
       </GridItem>
-      <GridItem  bg='#140d07' area={'nav'}>
-        <Text fontSize="xl">Nav</Text>
-        <Stack direction='row' spacing={4}>
-        <Button colorScheme="teal" mr="4">
-          Button 1
-        </Button>
-        
-        <Button leftIcon={<FaSignInAlt />} colorScheme='teal' variant='outline' p={6} fontSize="lg" _hover={{ bg: "teal.500", borderColor: "teal.500" }} _active={{ bg: "teal.700", borderColor: "teal.700" }} _focus={{ boxShadow: "outline" }}>
-          Login
-        </Button>
-        </Stack>
+      <GridItem bg='#140d07' area={'nav'} display="flex" flexDirection="column" justifyContent="center" alignItems="center">
+        <Flex direction="column" justifyContent="center" alignItems="center" alignSelf="center" mb={4}>
+          <Text fontSize="xl" mb='auto' color="white">Nav</Text>
+          <Stack direction='row' spacing={4}>
+            <Button
+              leftIcon={<FaUserPlus />}
+              colorScheme="teal"
+              variant='outline'
+              p={4}
+              fontSize="lg"
+              borderRadius="md"
+              _hover={{ bg: "teal.500", borderColor: "teal.500", boxShadow: "lg" }}
+              _active={{ bg: "teal.700", borderColor: "teal.700", boxShadow: "md" }}
+              _focus={{ boxShadow: "outline" }}
+            >
+              Sign-Up
+            </Button>
 
+            <Button
+              leftIcon={<FaSignInAlt />}
+              colorScheme='teal'
+              variant='outline'
+              p={4}
+              fontSize="lg"
+              borderRadius="md"
+              _hover={{ bg: "teal.500", borderColor: "teal.500", boxShadow: "lg" }}
+              _active={{ bg: "teal.700", borderColor: "teal.700", boxShadow: "md" }}
+              _focus={{ boxShadow: "outline" }}
+            >
+              Login
+            </Button>
+          </Stack>
+        </Flex>
       </GridItem>
-      <GridItem  bg='#140d07' area={'main'}>
-      <Flex justifyContent="center" alignItems="center" height="100%">
-      <Image src={logo} alt="Pallet Pro" boxSize="400px" />
-      </Flex>
+      <GridItem bg='#140d07' area={'main'}>
+        <Flex justifyContent="center" alignItems="center" height="100%">
+          <Image src={logo} alt="Pallet Pro" boxSize="400px" />
+        </Flex>
       </GridItem>
     </Grid>
   )
