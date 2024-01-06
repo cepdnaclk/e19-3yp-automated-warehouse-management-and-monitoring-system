92e6a8376a8a80d942bdaaec8a022d9003a2391c
e19363@eng.pdn.ac.lk <e19363@eng.pdn.ac.lkgit config --global user.email e19363@eng.pdn.ac.lk.>
2024-01-06 15:41:30 +0530

    Ultrasonic sensor + servo

--- code/Ultra sonic sensor/src/main.cpp
+++ code/Ultra sonic sensor/src/main.cpp
@@ -13,6 +13,8 @@ bool facingRight = false;
 bool objectDetectedRight = false; 
 bool facingLeft = false; 
 bool objectDetectedLeft = false;
+bool running = true;
+
 
 long duration;
 int distance;
@@ -22,7 +24,7 @@ void setup() {
 
   pinMode(trigPin, OUTPUT);
   pinMode(echoPin, INPUT);
-  // pinMode(LED_BUILTIN, OUTPUT);
+    
   myServo.write(90);  
   Serial.begin(9600);
 
@@ -30,64 +32,88 @@ void setup() {
 }
 
 int cal_distance(int duration){
+
     digitalWrite(trigPin, LOW);
     delayMicroseconds(2);
     digitalWrite(trigPin, HIGH);
     delayMicroseconds(10);
     digitalWrite(trigPin, LOW);
-     distance = duration * 0.034 / 2;
-     return distance;
+    duration = pulseIn(echoPin, HIGH);
+    distance = duration * 0.034 / 2;
+    return distance;
 }
 
 void loop() {
-  
-  
+  if(running){
 
-  duration = pulseIn(echoPin, HIGH);
-  distance = cal_distance(duration);
+    distance = cal_distance(duration);
 
-  Serial.print("Distance: ");
-  Serial.println(distance);
+    Serial.print("Distance: ");
+    Serial.println(distance);
     
-  if (distance < minimum && distance > 5) {
-    objectDetectedRight = true;
-    myServo.write(180);            // Move servo to right
-    delay(100);
-    facingRight = true;
-    duration = pulseIn(echoPin, HIGH);
-    distance = cal_distance(duration);
-    if(distance < minimum && distance > 5) {
+    if (distance < minimum && distance > 2) {
+      Serial.println("Object Detected at front. Turning Right");
       objectDetectedRight = true;
-    }else{
-      objectDetectedRight = false;
+      myServo.write(180);     // Move servo to right
+      facingRight = true;
+                 
+      delay(5000);
+
+      distance = cal_distance(duration);
+
+      if(distance < minimum && distance > 2) {
+        Serial.println("Object Detected at Right. Turning Left");
+        objectDetectedRight = true;
+        myServo.write(0);     // Move servo to left
+        delay(5000);
+
+        distance = cal_distance(duration);
+
+        if (distance < minimum && distance > 2) {
+          Serial.println("Object Detected at LEFT.");
+          objectDetectedLeft = true;
+          facingLeft = true;
+          myServo.write(90);     // Move servo to front
+          delay(5000);
+          Serial.println("Stopping....");
+          running = false;
+
+
+        }else{
+          Serial.println("Object Not Detected at LEFT.");
+          objectDetectedLeft = false;
+          myServo.write(90);    // Move servo to front
+          delay(5000);
+        }
+      }else{
+        Serial.println("Object Not Detected at Right.");
+        objectDetectedRight = false;
+        myServo.write(90);    // Move servo to front     
+        delay(5000);
+      }
+
     }
-  }
+    
+    
+    delay(200);
 
-  if(facingRight && !objectDetectedRight){
-    myServo.write(90);
-  }
 
-  if(facingRight && objectDetectedRight) {
-    objectDetectedLeft = true;
-    myServo.write(0);
-    delay(100);
-  //   myServo.write(45);
-  //   delay(100);
-    duration = pulseIn(echoPin, HIGH);
+    }
+
+  else{
     distance = cal_distance(duration);
-    if(distance < minimum && distance > 5){
-      objectDetectedLeft = true;
-    }else{
-      objectDetectedLeft = false;
+    if (distance < minimum && distance > 2){
+      running = false;
     }
+    else{
+      running = true;
+    }
+
+
+
   }
-  if(facingLeft && !objectDetectedLeft){
-    myServo.write(90);
-  }
-  // if(objectDetectedLeft && objectDetectedRight){
-  //   Serial.println("Object detected in both directions");
-  //   return;
-  // }
-  // delay(500);
+
+  
+
 }
 
